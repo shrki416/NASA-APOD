dddd18017d8aeda54671af3c0979b69f84c4dc5f
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-11-23 17:56:50 -0500

    update useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const getPhotoOfTheDay = useRef(() => {});
+
+  const API_KEY = `A4ACJGPvwBJhrAH9QuRTXnUku7LeLcZv8JFd0AiP`;
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+
+  getPhotoOfTheDay.current = () => {
+    axios
+      .get(url)
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getPhotoOfTheDay.current();
+  }, [getPhotoOfTheDay]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>NASA</h1>
+      <h2>Astronomy Picture of the Day</h2>
+      <p>{data.copyright}</p>
+      <p>{data.date}</p>
+      <p>{data.title}</p>
+      <img src={data.hdurl} alt={data.title} />
+      <p>{data.explanation}</p>
     </div>
   );
-}
+};
 
 export default App;
